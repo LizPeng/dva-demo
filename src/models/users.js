a988a994746863dce570325f5918f5919e129f69
LizPeng <lizhipeng2013@163.com>
2017-09-26 16:16:52 +0800

    step5

--- src/models/users.js
+++ src/models/users.js
@@ -13,15 +13,15 @@ export default {
   },
   effects: {
     *fetch({ payload: { page } }, { call, put }) {
-      const { data, headers } = yield call(usersService.ftech, { page });
+      const { data, headers } = yield call(usersService.fetch, { page });
       yield put({ type: 'save', payload: { data, total: headers['x-total-count'] } });
     },
   },
   subscriptions: {
     setup({ dispatch, history }) {
       return history.listen(({ pathname, query }) => {
-        if (pathname === '/user') {
-          dispatch({ type: 'ftech', payload: query });
+        if (pathname === '/users') {
+          dispatch({ type: 'fetch', payload: query });
         }
       });
     },
